a47ff7c0841e3b7e06ec5f45ef6f9c8855185353
rahulssv <rahulvishwakarma1181@gmail.com>
2023-11-29 07:25:35 +0530

    update

---
+++ run.sh
@@ -0,0 +1,7 @@
+docker build -t container1-image -f Dockerfile1 .;
+docker build -t container2-image -f Dockerfile2 .;
+docker network create mynetwork;
+docker run --name container1 --network mynetwork -d container1-image;
+docker run --name container2 --network mynetwork -d container2-image;
+
+
